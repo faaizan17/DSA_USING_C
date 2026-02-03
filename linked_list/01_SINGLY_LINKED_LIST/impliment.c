726c48e5b9796d4b79ea63eba7751529ea2f3a7d
faaizan17 <ahangarfaizan123@gmail.com>
2026-02-03 18:04:19 +0530

    modified printing function

---
+++ linked_list/01_SINGLY_LINKED_LIST/impliment.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{                //nickmane of "struct node" = node.
+    int data;
+    struct node *next;
+} node;
+node *Build123();
+node *getnode(int x);
+void print(node *s);
+
+node *HP;               // head pointer
+/*Build123 creates a list 1->2->3  and returns adress of first node*/
+node* Build123(){
+    node *temp1, *temp2, *temp3;
+    temp1 = getnode(1);
+    temp2 = getnode(2);
+    temp3 = getnode(3);
+    temp1-> next = temp2;
+    temp2 -> next= temp3;
+    return temp1;
+}
+/*getnode creates a node: assigns x to data part and NULL to next part and returns adrress of node*/                      
+node* getnode(int x){                       // returs a structure pointer {pointer variable}
+    node *ptr;            
+    ptr = (node *)malloc(sizeof(node));
+    ptr -> data = x;
+    ptr -> next= NULL;
+    return ptr;
+}
+void print(node* s){            // adress of first node
+    while(s != NULL){
+        printf("data : %d \t", s->data);
+        s = s->next;
+    }
+}
+int main(){
+    HP = Build123();
+    print(HP);
+    return 0;
+}
