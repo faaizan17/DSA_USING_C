6d300671b7f246ec14c1bfe80a795c66254e81c5
faaizan17 <ahangarfaizan123@gmail.com>
2026-02-07 11:44:50 +0530

    delete first node

---
+++ linked_list/02_DOUBLY_LINKED_LIST/06_delete_begin.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+} node;
+node *HP;
+node* getnode(int x){
+    node *ptr = (node *)malloc(sizeof(node));
+    ptr->data = x;
+    ptr->next = NULL;
+    ptr->prev = NULL;
+    return ptr;
+}
+node* Build(){
+    node *temp1, *temp2, *temp3;
+    temp1 = getnode(1);
+    temp2 = getnode(2);
+    temp3 = getnode(3);
+    temp1->next = temp2;
+    temp2->next = temp3;
+    temp3->prev = temp2;
+    temp2->prev = temp1;
+    return temp1;
+}
+void print1(){
+    node *s = HP;
+    printf("Linked List Data :\n");
+    while(s!=NULL){
+        printf("%d\t", s->data);
+        s = s->next;
+    }
+    printf("\n");
+}
+void print2(){
+    node *s = HP;
+    while(s->next!=NULL){
+        s = s->next;
+    }
+    printf("Linked List Data in Reverse Order : \n");
+    while(s!=NULL){
+        printf("%d\t", s->data);
+        s = s->prev;
+    }
+    printf("\n");
+}
+void delbegin(){
+    node *temp = HP;
+    if(HP==NULL){
+        printf("list is already empty\n");
+        return;
+    }
+    if(HP->next==NULL){
+        free(HP);
+        HP = NULL;
+        return;
+    }
+    HP = HP->next;
+    HP->prev = NULL;
+    free(temp);
+}
+void freelist(node* s){
+    node *temp;
+    while(s!=NULL){
+        temp = s;
+        s = s->next;
+        free(temp);
+    }
+}
+int main(){
+    HP = Build();
+    print1();
+    print2();
+    print1();
+    delbegin();
+    print1();
+    delbegin();
+    print1();
+    delbegin();
+    print1();
+    delbegin();
+    freelist(HP);
+    return 0;
+}
