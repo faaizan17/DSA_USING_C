ae0b20d8f2e5f23c9684f65b0a4b61c4eb1af28a
faaizan17 <ahangarfaizan123@gmail.com>
2026-02-07 12:00:07 +0530

    delete last element of list:

---
+++ linked_list/02_DOUBLY_LINKED_LIST/07_delete_last.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+} node;
+node *HP;
+node* getnode(int x){
+    node *ptr = (node *)malloc(sizeof(node));
+    ptr->data = x;
+    ptr->next = NULL;
+    ptr->prev = NULL;
+    return ptr;
+}
+node* Build(){
+    node *temp1, *temp2, *temp3;
+    temp1 = getnode(1);
+    temp2 = getnode(2);
+    temp3 = getnode(3);
+    temp1->next = temp2;
+    temp2->next = temp3;
+    temp3->prev = temp2;
+    temp2->prev = temp1;
+    return temp1;
+}
+void print1(){
+    node *s = HP;
+    printf("Linked List Data :\n");
+    while(s!=NULL){
+        printf("%d\t", s->data);
+        s = s->next;
+    }
+    printf("\n");
+}
+void print2(){
+    node *s = HP;
+    while(s->next!=NULL){
+        s = s->next;
+    }
+    printf("Linked List Data in Reverse Order : \n");
+    while(s!=NULL){
+        printf("%d\t", s->data);
+        s = s->prev;
+    }
+    printf("\n");
+}
+void delend(){
+    if(HP==NULL){
+        printf("You can not delete elemet, LIst is alredy empty");
+        return;
+    }
+    if(HP->next==NULL){
+        free(HP);
+        HP = NULL;
+        return;
+    }
+    node *s = HP;
+    // node *temp = NULL;
+    while(s->next!=NULL){
+        // temp = s;
+        s = s->next;
+    }
+    s->prev->next = NULL;
+    // s->prev = NULL;
+    free(s);
+}
+void freelist(node* s){
+    node *temp;
+    while(s!=NULL){
+        temp = s;
+        s = s->next;
+        free(temp);
+    }
+}
+int main(){
+    HP = Build();
+    print1();
+    print2();
+    delend();
+    print1();
+    freelist(HP);
+    return 0;
+}
