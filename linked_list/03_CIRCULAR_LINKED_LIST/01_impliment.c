23112ce495a546dc9abcda2f1467049eae9c029b
faaizan17 <ahangarfaizan123@gmail.com>
2026-02-07 18:17:09 +0530

    circular LL implimentation

---
+++ linked_list/03_CIRCULAR_LINKED_LIST/01_impliment.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int data;
+    struct node *next;
+} node;
+node *HP = NULL;
+node* getnode(int x){
+    node *ptr = (node *)malloc(sizeof(node));
+    ptr->data = x;
+    ptr->next = NULL;
+    return ptr;
+}
+node* build(){
+    node *temp1, *temp2, *temp3;
+    temp1 = getnode(1);
+    temp2 = getnode(2);
+    temp3 = getnode(3);
+    temp1->next = temp2;
+    temp2->next = temp3;
+    temp3->next = temp1;
+    return temp1;
+}
+void print(){
+    node *s = NULL;
+    printf("linked list data :\n");
+    while(s!=HP){
+        if(s==NULL){
+            s = HP;
+            printf("%d\t", s->data);
+            s = s->next;
+            continue;
+        }
+        printf("%d\t", s->data);
+        s = s->next;
+    }
+}
+void freelist(){
+    node *temp = NULL;
+    node *s = HP;
+    while(s->next!=HP){
+        temp = s;
+        s = s->next;
+        free(temp);
+    }
+}
+int main(){
+    HP = build();
+    print();
+    freelist();
+    return 0;
+}
